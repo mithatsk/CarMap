06528356c134f7180bf8822515b17925bb94ad1d
mithatsk <mithatkaskara@gmail.com>
2020-12-26 12:45:56 +0300

    Initialized the project

---
+++ CarMap/CarMap/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CarMap
+//
+//  Created by mithat samet kaskara on 26.12.2020.
+//  Copyright © 2020 Mithat Kaskara. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ CarMap/CarMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarMap
+//
+//  Created by mithat samet kaskara on 26.12.2020.
+//  Copyright © 2020 Mithat Kaskara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
