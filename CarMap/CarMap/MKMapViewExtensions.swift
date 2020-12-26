50dd17a78185a66e74627180fb2399368ef2b6f3
mithatsk <mithatkaskara@gmail.com>
2020-12-27 02:13:00 +0300

    Added an extension method for centering the map to specified location

---
+++ CarMap/CarMap/MKMapViewExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  MKMapViewExtensions.swift
+//  CarMap
+//
+//  Created by mithat samet kaskara on 26.12.2020.
+//  Copyright © 2020 Mithat Kaskara. All rights reserved.
+//
+
+import MapKit
+
+extension MKMapView {
+    
+    func centerToLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 5000) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius,
+                                                  longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+    
+}
