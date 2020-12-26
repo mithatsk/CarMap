a8e8afd7c7cc22df457e2e627a693ce513a40d5e
mithatsk <mithatkaskara@gmail.com>
2020-12-26 13:46:24 +0300

    Added app colors

---
+++ CarMap/CarMap/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  CarMap
+//
+//  Created by mithat samet kaskara on 26.12.2020.
+//  Copyright © 2020 Mithat Kaskara. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let dark = UIColor(hexString: "#585858")
+    static let darkBlack = UIColor(hexString: "#404040")
+    static let lightGray = UIColor(hexString: "#f6f7fb")
+    static let yellow = UIColor(hexString: "#ffcc00")
+    static let white = UIColor(hexString: "#ffffff")
+}
