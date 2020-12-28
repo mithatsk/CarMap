558e72f97014f21c3db8599b8224f280fdf306aa
mithatsk <mithatkaskara@gmail.com>
2020-12-28 20:35:35 +0300

    Added target for unit test

---
+++ CarMapTests/CarMapTests.swift
@@ -0,0 +1,18 @@
+//
+//  CarMapTests.swift
+//  CarMapTests
+//
+//  Created by mithat samet kaskara on 28.12.2020.
+//  Copyright © 2020 Mithat Kaskara. All rights reserved.
+//
+
+import XCTest
+@testable import CarMap
+
+class CarMapTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+}
